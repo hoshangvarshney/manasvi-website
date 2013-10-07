29e62498cee644a9f6f10a31f5574d5b9e17cb20
hoshang.varshney@gmail.com <hoshang.varshney@gmail.com@34f31126-2a6e-46aa-48d9-c05765a41369>
2013-10-07 15:01:41 +0000

    initial import

---
+++ trunk/src/com/website/servlet/server/SendEmailServlet.java
@@ -0,0 +1,70 @@
+package com.website.servlet.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SendEmailServlet extends HttpServlet
+{
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		doPost(req, resp);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		String senderName = req.getParameter("sndName");
+		String senderEmail = req.getParameter("sndEmail");
+		String message = req.getParameter("msg");
+		
+		message="Sender Name: "+ senderName+"\nSender Email: "+senderEmail+ "\n\n"+message;
+
+		String response = "OK|Success";
+
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+
+		try
+		{
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress("hoshang.varshney@gmail.com", "Manasvi Website"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress("arnavi.varshney@gmail.com", "Arnavi Varshney"));
+			msg.setSubject("Message from " + senderName + " on "+(new Date(System.currentTimeMillis()).toString()));
+			msg.setText(message);
+			Transport.send(msg);
+
+		}
+		catch (AddressException e)
+		{
+			response = "ERR|ADR001";
+		}
+		catch (MessagingException e)
+		{
+			response = "ERR|MSG001";
+			e.printStackTrace();
+		}
+		finally
+		{
+			PrintWriter out = resp.getWriter();
+			out.print(response);
+			out.flush();
+		}
+	}
+}
